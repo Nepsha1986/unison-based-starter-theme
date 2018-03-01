d2065af579d257a56651c3ae5281b62258819776
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-03-02 00:15:12 +0200

    Created  build for unison components assets

--- functions.php
+++ functions.php
@@ -121,6 +121,10 @@ function unison_based_theme_scripts() {
 
 	wp_enqueue_script( 'unison-based-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js');
+
+	wp_enqueue_script( 'unison-components', get_template_directory_uri() . '/js/unison-components.js');
+
 	wp_enqueue_script( 'unison-based-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
 let mix = require('laravel-mix');
 
+// TODO: fix or change build ( currently 'npm run watch command is not working correctly')
+mix.babel(['framework-customizations/extensions/shortcodes/shortcodes/*/views/*.js'], 'js/unison-components.js');
+mix.combine(['framework-customizations/extensions/shortcodes/shortcodes/*/views/*.scss'], 'temp/unison-components.scss');
+
 mix.sass('styles/all.scss', 'styles');
