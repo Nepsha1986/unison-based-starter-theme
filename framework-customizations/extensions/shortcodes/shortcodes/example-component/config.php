3cb0b04e34812cafa8723104ec726e9b6eb0879c
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-03-10 22:50:23 +0200

    Example componet emprovements

---
+++ framework-customizations/extensions/shortcodes/shortcodes/example-component/config.php
@@ -0,0 +1,11 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+$cfg = array(
+    'page_builder' => array(
+        'title' => __('Example Section Component', 'ust'),
+        'description' => __('Example component', 'ust'),
+        'tab' => __('Sections', 'ust'),
+        'popup_size' => 'medium',
+        'icon' => 'dashicons dashicons-groups',
+    )
+);
