40bce473b9dd7f3c86ba19059fd80929e870f263
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-03-10 23:05:52 +0200

    Added containers to layouts

--- single.php
+++ single.php
@@ -9,27 +9,26 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main container">
-
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() ); ?>
-
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
             <div class="container">
-                <?php the_post_navigation();
+                <?php
+                while (have_posts()) : the_post();
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+                    get_template_part('template-parts/content', get_post_type()); ?>
+
+                    <?php the_post_navigation();
+
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if (comments_open() || get_comments_number()) :
+                        comments_template();
+                    endif;
 
                 endwhile; // End of the loop.
                 ?>
             </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_sidebar();
