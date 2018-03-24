8ec5b95bd2590b72152277fb0d96aad7c607842d
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-03-24 21:00:22 +0200

    Fixed problem with build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
 let mix = require('laravel-mix');
 
-// TODO: fix or change build ( currently 'npm run watch command is not working correctly')
 mix.babel(['framework-customizations/extensions/shortcodes/shortcodes/*/views/*.js'], 'js/unison-components.js');
-mix.combine(['framework-customizations/extensions/shortcodes/shortcodes/*/views/*.scss'], 'temp/unison-components.scss');
 
-mix.sass('styles/all.scss', 'styles');
+mix.webpackConfig({
+    module: {
+        rules: [{
+            test: /\.scss/,
+            enforce: "pre",
+            loader: 'import-glob-loader'
+        }]
+    }
+}).sass('styles/all.scss', 'styles');
