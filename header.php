b232ef188b4e48808ecf7365eaa1121d057ed253
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-04-04 00:23:44 +0300

    Header styles changes, addede variables, defaults changes

--- header.php
+++ header.php
@@ -17,7 +17,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
